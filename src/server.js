bd9b069d2b8ccf988a3d5ff176ba0dfc3b52c3de
Eli Davis <eli.davis1995@gmail.com>
2017-04-08 07:27:43 -0500

    Allows cross site scripting for ease

--- src/server.js
+++ src/server.js
@@ -8,72 +8,79 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('database.db');
 
 app.use(bodyParser.json());
-app.post('/sightings', function(req, res) {
-	console.log("POST /");
-	var data = req.body;
 
-	db.serialize(function () {
-	  db.run('CREATE TABLE IF NOT EXISTS persons (latitude REAL, longitude REAL, timestamp REAL, picture BLOB, extrainfo TEXT)');
-	  var stmt = db.prepare("INSERT INTO persons VALUES (?, ?, ?, ?, ?)");
-		stmt.run(data.lat, data.lon, data.timestamp, data.picture, data.extrainfo);
-	  stmt.finalize();
-	});
-	db.close()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.post('/sightings', function(req, res) {
+    console.log("POST /");
+    var data = req.body;
+
+    db.serialize(function() {
+        db.run('CREATE TABLE IF NOT EXISTS persons (latitude REAL, longitude REAL, timestamp REAL, picture BLOB, extrainfo TEXT)');
+        var stmt = db.prepare("INSERT INTO persons VALUES (?, ?, ?, ?, ?)");
+        stmt.run(data.lat, data.lon, data.timestamp, data.picture, data.extrainfo);
+        stmt.finalize();
+    });
+    db.close(function() {
+        res.send({ status: "success" });
+    });
 });
 
 app.get('/sightings', function(req, res) {
-	console.log("GET /");
-	var query = req._parsedUrl.query;
-	if (query != null) {
-		var db = new sqlite3.Database('database.db');
-		console.log("Send specific query.");
-		var queryString = query.split('&');
-		var databaseQuery = "";
+    console.log("GET /");
+    var query = req._parsedUrl.query;
+    if (query != null) {
+        var db = new sqlite3.Database('database.db');
+        console.log("Send specific query.");
+        var queryString = query.split('&');
+        var databaseQuery = "";
 
-		for (var i = 0; i < queryString.length; i++) {
-			var query = queryString[i].split('=');
-			if (databaseQuery.length != 0) {
-				databaseQuery += " AND ";
-			}
+        for (var i = 0; i < queryString.length; i++) {
+            var query = queryString[i].split('=');
+            if (databaseQuery.length != 0) {
+                databaseQuery += " AND ";
+            }
 
-			if (query[0] == 'lat') {
-				databaseQuery += 'latitude=' + query[1].toString();
-			}
-			else if (query[0] == 'lon') {
-				databaseQuery += 'longitude=' + query[1].toString();
-			}
-			else if (query[0] == 'radius') {
-				databaseQuery += 'radius=' + query[1].toString();
-			}
-		}
+            if (query[0] == 'lat') {
+                databaseQuery += 'latitude=' + query[1].toString();
+            } else if (query[0] == 'lon') {
+                databaseQuery += 'longitude=' + query[1].toString();
+            } else if (query[0] == 'radius') {
+                databaseQuery += 'radius=' + query[1].toString();
+            }
+        }
 
-		databaseQuery = "SELECT * FROM persons WHERE " + databaseQuery + ";";
-		console.log(databaseQuery);
-		values = [];
-		db.each(databaseQuery, function (err, row) {
-				values.push(row);
-		});
+        databaseQuery = "SELECT * FROM persons WHERE " + databaseQuery + ";";
+        console.log(databaseQuery);
+        values = [];
+        db.each(databaseQuery, function(err, row) {
+            values.push(row);
+        });
 
-		db.close(function () {
-			res.send(values);
-			console.log(values);
-		});
-	}
-	else {
-		console.log("Send everything.");
-		var db = new sqlite3.Database('database.db');
-		values = []
-		db.each('SELECT * FROM persons', function (err, row) {
-				values.push(row);
-		});
+        db.close(function() {
+            res.send(values);
+            console.log(values);
+        });
+    } else {
+        console.log("Send everything.");
+        var db = new sqlite3.Database('database.db');
+        values = []
+        db.each('SELECT * FROM persons', function(err, row) {
+            values.push(row);
+        });
 
-		db.close(function () {
-			res.send(values);
-			console.log(values);
-		});
-	}
+        db.close(function() {
+            res.send(values);
+            console.log(values);
+        });
+    }
 });
 
 app.listen(3000, function() {
-	console.log("Server is listening...");
+    console.log("Server is listening on: ", 3000);
 });
